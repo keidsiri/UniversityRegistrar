edaaf838af22ab11db10af6cf4b655629d5ee033
Kate Kiatsiri <keidsiri@gmail.com>
2022-01-04 10:41:46 -0800

    Add Edit , AddCourse and Delete ()  in studentscontroller.cs
    Co-authored-by: Karen Axon <kaxonwa@gmail.com>
    Co-authored by: Ella Tanttu <ellajtanttu@gmail.com>

--- UniversityRegistrar/Controllers/StudentsController.cs
+++ UniversityRegistrar/Controllers/StudentsController.cs
@@ -20,5 +20,85 @@ namespace UniversityRegistrar.Controllers
     {
       return View(_db.Students.ToList());
     }
+
+    public ActionResult Create()
+    {
+      ViewBag.CourseId = new SelectList(_db.Courses, "CourseId", "Name");
+      return View();
+    }
+
+    [HttpPost]
+    public ActionResult Create(Student student, int CourseId)
+    {
+      _db.Students.Add(student);
+      _db.SaveChanges();
+      if (CourseId != 0)
+      {
+        _db.CourseStudent.Add(new CourseStudent() { CourseId = CourseId, StudentId = student.StudentId });
+      }
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    public ActionResult Details(int id)
+    {
+      var thisStudent = _db.Students
+      .Include(student => student.JoinEntities)
+      .ThenInclude(join => join.Course)
+      .FirstOrDefault(student => student.StudentId == id);
+      return View(thisStudent);
+    }
+
+    public ActionResult Edit(int id)
+    {
+      var thisStudent = _db.Students.FirstOrDefault(student => student.StudentId == id);
+      ViewBag.CourseId = new SelectList(_db.Courses, "CourseId", "Name");
+      return View(thisStudent);
+    }
+
+    [HttpPost]
+    public ActionResult Edit(Student student, int CourseId)
+    {
+      if (CourseId != 0)
+      {
+        _db.CourseStudent.Add(new CourseStudent() { CourseId = CourseId, StudentId = student.StudentId });
+      }
+      _db.Entry(student).State = EntityState.Modified;
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    public ActionResult AddCourse(int id)
+    {
+      var thisStudent = _db.Students.FirstOrDefault(student => student.StudentId == id);
+      ViewBag.CourseId = new SelectList(_db.Courses, "CourseId", "Name");
+      return View(thisStudent);
+    }
+
+    [HttpPost]
+    public ActionResult AddCourse(Student student, int CourseId)
+    {
+      if (CourseId != 0)
+      {
+      _db.CourseStudent.Add(new CourseStudent() { CourseId = CourseId , StudentId = student.StudentId });
+      }
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    public ActionResult Delete(int id)
+    {
+      var thisStudent = _db.Students.FirstOrDefault(student => student.StudentId == id);
+      return View(thisStudent);
+    }
+
+    [HttpPost, ActionName("Delete")]
+    public ActionResult DeleteConfirmed(int id)
+    {
+      var thisStudent = _db.Students.FirstOrDefault(student => student.StudentId == id);
+      _db.Students.Remove(thisStudent);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
   }
 }
--- UniversityRegistrar/Models/Student.cs
+++ UniversityRegistrar/Models/Student.cs
@@ -1,6 +1,6 @@
 using System.Collections.Generic;
-// using System.DateTime;
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace UniversityRegistrar.Models
 {
@@ -14,6 +14,9 @@ namespace UniversityRegistrar.Models
         public int StudentId { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString="{0:yyyy-MM-dd}", ApplyFormatInEditMode=true)]
         public DateTime DateEnrollment { get; set; }
 
         public virtual ICollection<CourseStudent> JoinEntities { get;}
